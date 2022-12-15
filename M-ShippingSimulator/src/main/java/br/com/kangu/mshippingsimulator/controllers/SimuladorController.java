8afa5a4186a620f86cbb5f6968fa78130b6614b0
Victor Hugo Siqueira Gomes <victor.s.gomes03@gmail.com>
2022-12-15 17:40:57 -0300

    Gerando PDF, falta descobrir como gerar menos bugado

--- M-ShippingSimulator/src/main/java/br/com/kangu/mshippingsimulator/controllers/SimuladorController.java
+++ M-ShippingSimulator/src/main/java/br/com/kangu/mshippingsimulator/controllers/SimuladorController.java
@@ -1,11 +1,18 @@
 package br.com.kangu.mshippingsimulator.controllers;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import okhttp3.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/simulador")
@@ -17,17 +24,54 @@ public class SimuladorController {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(mediaType, "{\r\n  \"cepOrigem\": \"01010010\",\r\n  \"cepDestino\": \"13472500\",\r\n  \"vlrMerc\": \"20.50\",\r\n  \"pesoMerc\": \"0.040\",\r\n  \"volumes\": [\r\n    {\r\n      \"peso\": \"0.040\",\r\n      \"altura\": \"2\",\r\n      \"largura\": \"11\",\r\n      \"comprimento\": \"16\"\r\n    }\r\n  ],\r\n   \"servicos\": [\r\n    \"E\", \"X\", \"R\", \"M\"\r\n  ]\r\n}");
+
+        RequestBody body = RequestBody.create(mediaType, "{" +
+                                                                    "\"cepOrigem\": \"01010010\"," +
+                                                                    "\"cepDestino\": \"13472500\"," +
+                                                                    "\"vlrMerc\": \"20.50\"," +
+                                                                    "\"pesoMerc\": \"0.040\"," +
+                                                                    "\"volumes\": [" +
+                                                                        "{" +
+                                                                        "\"peso\": \"0.040\"," +
+                                                                        "\"altura\": \"2\"," +
+                                                                        "\"largura\": \"11\"," +
+                                                                        "\"comprimento\": \"16\"" +
+                                                                        "}" +
+                                                                    "]," +
+                                                                    "\"servicos\": [" +
+                                                                        "\"E\", \"X\", \"R\", \"M\"" +
+                                                                    "]" +
+                                                                "}");
+
         Request request = new Request.Builder()
                 .url("https://portal.kangu.com.br/tms/transporte/simular")
                 .method("POST", body)
                 .addHeader("token", "64845971de3d90c6f5f3e97eabe0e12b8500752fb695283b7c5466c1221e7d16")
                 .addHeader("Content-Type", "application/json")
-                .addHeader("Cookie", "KANGUSESSID=3kl96b3muuk38r37ia6d2lu3c5")
                 .build();
         Response response = client.newCall(request).execute();
 
         return response.body().string();
+    }
+
+    @GetMapping(value = "/teste")
+    public void testeJson() throws ParseException {
+
+        String jsonString = "{\n" +
+                "    \"Name\": \"crunchify.com\",\n" +
+                "    \"Author\": \"App Shah\",\n" +
+                "    \"Company List\": [\n" +
+                "        \"Compnay: eBay\",\n" +
+                "        \"Compnay: Paypal\",\n" +
+                "        \"Compnay: Google\"\n" +
+                "    ]\n" +
+                "}";
+
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(jsonString);
+
+       // Testes de json parse. FAIL
+
     }
 
 
---
+++ M-Tag/src/main/java/br/com/kangu/mtag/controllers/TagController.java
@@ -0,0 +1,47 @@
+package br.com.kangu.mtag.controllers;
+
+import com.lowagie.text.DocumentException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.*;
+
+@RestController
+@RequestMapping(value = "/tag")
+public class TagController {
+
+    @GetMapping
+    private void parseThymeleafTemplate() throws DocumentException, IOException {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("to", "Baeldung");
+
+        generatePdfFromHtml(templateEngine.process("thymeleaf_template", context));
+
+    }
+
+    public void generatePdfFromHtml(String html) throws IOException, DocumentException {
+        String outputFolder = System.getProperty("user.home") + File.separator + "thymeleaf.pdf";
+        OutputStream outputStream = new FileOutputStream(outputFolder);
+
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(html);
+        renderer.layout();
+        renderer.createPDF(outputStream);
+
+        outputStream.close();
+    }
+
+}
